ec644f13925051fac98aba4f5830b71378cc2188
kbaraniak <90936580+kbaraniak@users.noreply.github.com>
2026-05-10 16:28:40 +0200

    [Build] Add npm package configuration and Tailwind CSS setup

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./**/*.php",
+    "./**/*.html",
+    "./tests/**/*.html"
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
